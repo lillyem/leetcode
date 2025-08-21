ac9674ad4b7d5043b6956f5784a3c164b28f84f7
lillyem <sonali.maharaj2153@gmail.com>
2025-08-21 18:13:56 -0400

    Create max_number_of_ksum_pairs.py

---
+++ algorithms/arrays/max_number_of_ksum_pairs.py
@@ -0,0 +1,98 @@
+"""
+Source : https://leetcode.com/problems/max-number-of-k-sum-pairs?envType=study-plan-v2&envId=leetcode-75
+Author : Sonali Maharaj
+Date   : 21/08/2025
+
+****************************************************************************************************
+
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= 109
+
+****************************************************************************************************
+UMPIRE: 
+
+input: nums (integer array), k (integer)
+
+output: max_operations (integer)
+
+explanation: find two numbers from the array and check if its sum is equal to k, find the max number of operations thats possible
+
+edge cases:
+1. nums has 1 element
+2. num operations is 0
+
+psuedocode: 
+
+left = 0
+right = length of nums - 1
+num_operations = 0
+sort array
+
+while left is less than right:
+    sum = nums at left + nums at right
+    if sum = k
+      num_operations += 1
+      increment left
+      decrement right
+
+    if sum < k
+        increment left
+    elif sum > k 
+        decrement right
+
+return num_operations
+
+"""
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        num_operations = 0
+        nums.sort()
+
+        while left < right:
+            total = nums[left] + nums[right]
+            if total == k:
+                num_operations += 1
+                left+=1
+                right-=1
+            
+            if total < k:
+                left+=1
+            elif total > k: 
+                right-=1
+
+        return num_operations
